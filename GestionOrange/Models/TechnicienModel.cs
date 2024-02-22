fdac9a30332e57a0fd169730b28d80231b68912d
Lukas Goulois <lukas.goulois@bts-snir.com>
2024-02-22 10:58:53 +0100

    Ajout du modèle technicien

---
+++ GestionOrange/Models/TechnicienModel.cs
@@ -0,0 +1,25 @@
+﻿using SQLite;
+
+namespace GestionOrange.Models
+{
+    [Table("Technicien")]
+    public class TechnicienModel
+    {
+
+        [PrimaryKey, AutoIncrement, Column("idTechnicien")]
+        public int IdTechnicien { get; set; }
+
+        [Column("nom")]
+        public string NomTechnicien { get; set; }
+
+        [Column("prenom")]
+        public string PrenomTechnicien { get; set; }
+
+        [Column("numeroTelephone")]
+        public string NumeroTechnicien { get; set; }
+
+        [Column("astreinte")]
+        public bool Astreinte { get; set; }
+    }
+}
+
