042bf4198737a182f938373286ff29f1922d9357
Lukas Goulois <lukas.goulois@bts-snir.com>
2024-02-22 11:01:23 +0100

    Ajout du modèle secteur

---
+++ GestionOrange/Models/SecteurModel.cs
@@ -0,0 +1,14 @@
+﻿using SQLite;
+
+namespace GestionOrange.Models
+{
+    [Table("Secteur")]
+    public class SecteurModel
+    {
+        [PrimaryKey, AutoIncrement, Column("idSecteur")]
+        public int IdSecteur { get; set; }
+
+        [Column("nom")]
+        public string NomSecteur { get; set; }
+    }
+}
