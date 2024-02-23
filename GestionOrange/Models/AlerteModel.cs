db28cc81d7838a914192a0d61ed0563295f33d7f
Lukas Goulois <lukas.goulois@bts-snir.com>
2024-02-23 16:30:04 +0100

    Ajout du modèle Alerte

---
+++ GestionOrange/Models/AlerteModel.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+namespace GestionOrange.Models
+{
+    [Table("Alertes")]
+    public class AlerteModel
+    {
+        [PrimaryKey, AutoIncrement, Column("idAlerte")]
+        public int IdAlerte { get; set; }
+
+        [Column("date")]
+        public DateTime DateAlerte { get; set;}
+
+        [Column("Chambre_idChambre")]
+        public int Chambre_IdChambre { get; set; }
+
+        [Column("Technicien_idTechnicien")]
+        public int Technicien_IdTechnicien { get; set; }
+
+    }
+}
