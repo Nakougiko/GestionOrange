db8ac0618389e4f14f931b8a90c8dba016690046
Lukas Goulois <lukas.goulois@bts-snir.com>
2024-03-13 10:20:16 +0100

    - Ajout du crud

---
+++ GestionOrange/Services/DatabaseContext.cs
@@ -0,0 +1,63 @@
+﻿using SQLite;
+
+namespace GestionOrange.Services
+{
+    public class DatabaseContext
+    {
+        private const string DbName = "database.db3";
+        private static string DbPath => Path.Combine(FileSystem.AppDataDirectory, DbName);
+
+        private SQLiteAsyncConnection _connection;
+        private SQLiteAsyncConnection Database =>
+            (_connection ??= new SQLiteAsyncConnection(DbPath, SQLiteOpenFlags.Create | SQLiteOpenFlags.ReadWrite | SQLiteOpenFlags.SharedCache));
+
+        private async Task CreateTableIfNotExists<TTable>() where TTable : class, new()
+        {
+            await Database.CreateTableAsync<TTable>();
+        }
+
+        // Récupère la table spécifiée
+        private async Task<AsyncTableQuery<TTable>> GetTableAsync<TTable>() where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return Database.Table<TTable>();
+        }
+
+        // Récupère tous les éléments de la table spécifiée
+        public async Task<IEnumerable<TTable>> GetAllAsync<TTable>() where TTable : class, new()
+        {
+            var table = await GetTableAsync<TTable>();
+            return await table.ToListAsync();
+        }
+
+        // Exécute une action sur la table spécifiée
+        private async Task<TResult> Execute<TTable, TResult>(Func<Task<TResult>> action) where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await action();
+        }
+
+        // Ajoute un nouvel élément à la table spécifiée
+        public async Task<bool> AddItemAsync<TTable>(TTable item) where TTable : class, new()
+        {
+            return await Execute<TTable, bool>(async () => await Database.InsertAsync(item) > 0);
+        }
+
+        // Met à jour un élément dans la table spécifiée
+        public async Task<bool> UpdateItemAsync<TTable>(TTable item) where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await Database.UpdateAsync(item) > 0;
+        }
+
+        // Supprime un élément de la table spécifiée
+        public async Task<bool> DeleteItemAsync<TTable>(TTable item) where TTable : class, new()
+        {
+            await CreateTableIfNotExists<TTable>();
+            return await Database.DeleteAsync(item) > 0;
+        }
+
+        // Libère les ressources de connexion à la base de données
+        public async ValueTask DisposeAsync() => await _connection?.CloseAsync();
+    }
+}
